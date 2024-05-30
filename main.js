d1acb1a9bbd37086af56cfdd0b99f7611032da09
obongsik <zkfkdzhdo@naver.com>
2024-05-30 16:21:34 +0900

    ejs로 변경

--- main.js
+++ main.js
@@ -1,32 +1,42 @@
 "use strict";
 
-const fs = require("fs"),
-  port = 80,
-  http = require("http"),
-  httpStatus = require("http-status-codes"),
-  router = require("./router"),
-  plainTextContentType = {
-    "Content-Type":"text/plain"
-  },
-  htmlContentType = {
-    "Content-Type":"text/html"
-  },
-  customReadFile = (file, res) => {
-    fs.readFile(`./${file}`, (errors, data) => {
-          if (errors) {
-            console.log("Error reading the file...");
-          }
-          res.end(data);
-    });
-  };
-router.get("/", (req, res) => {
-    res.writeHead(httpStatus.OK, htmlContentType);
-    customReadFile("views/userHome.html", res);
-});
-/*router.get("/userMain", (req, res) => {
-  res.writeHead(httpStatus.OK, htmlContentType);
-  customReadFile("views/userMain.html", res);
-});*/
+const express = require("express"),
+    app = express(),
+    path = require('path'),
+    homeController = require("./controllers/homeController"),
+    errorController = require("./controllers/errorController"),
+    layouts = require("express-ejs-layouts"),
+    db = require("./models/index"),
+    Sequelize = db.Sequelize,
+    Op = Sequelize.Op;
 
-http.createServer(router.handle).listen(port);
-console.log(`The server has started and is listening on port number: ${port}`);
+    app.set("port", process.env.PORT || 80);
+app.set("view engine", "ejs"); // 애플리케이션 뷰 엔진을 ejs로 설정
+app.set('views', path.join(__dirname, 'views'));
+// 정적 뷰 제공
+app.use(express.static("public"));
+// 레이아웃 설정
+//app.use(layouts);
+// 데이터 파싱
+app.use(
+    express.urlencoded({
+        extended:false
+    })
+);
+app.use(express.json());
+
+// 라우트 등록
+/*app.get("/subscribers/getSubscriber", subscriberController.getAllSubscribers);
+app.get("/subscribers/subscriber", subscriberController.getSubscriptionPage); // 폼 입력이 가능한 웹 페이지 렌더링
+app.post("/subscribers/subscribe", subscriberController.saveSubscriber); // 넘겨받은 POST 데이터 저장 및 처리*/
+app.get("/", homeController.showIndex);
+
+app.use(errorController.logErrors);
+app.use(errorController.respondNoResourceFound);
+app.use(errorController.respondInternalError);
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
+app.listen(app.get("port"), () => {
+    console.log(`Server running on port: ${app.get("port")}`);
+})
+
